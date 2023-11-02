585055efcf1da523d9c72ec459b9c026a0b06f68
johnvdpk <johnvdpk@gmail.com>
2023-11-02 12:36:00 +0100

    comp. Form DIY gemaakt dankzij sjon

--- src/pages/DataPages/DataPageJobSeeker/DataPage.jsx
+++ src/pages/DataPages/DataPageJobSeeker/DataPage.jsx
@@ -1,6 +1,9 @@
 import './DataPage.css'
 import Form from "../../FormData/Form/Form.jsx";
-
+import profilefoto from "../../../assets/profilefoto.png"
+import Button from "../../globalcomponents/Buttons/Button.jsx";
+import formConfigJobSeeker from "../../FormData/Form/JsonDataForm/formProfileJobseeker.json"
+import formConfigEmployer from "../../FormData/Form/JsonDataForm/formProfileEmployer.json"
 
 
 function DataPage() {
@@ -13,10 +16,26 @@ function DataPage() {
 
        <div className="data-page-wrapper">
 
-           <div className="div-personal-data-form">
+           <div className="div-data-page-menu">
+                <div className="div-profile-foto">
+                    <img src={profilefoto} className="img-profile-foto" alt="profile foto" />
+                </div>
+                <div className="div-data-page-menu-child">
+                    <Button />
+                    <Button />
 
-               <Form/>
 
+
+                </div>
+
+           </div>
+
+           <div className="div-personal-form">
+               <Form formConfig={formConfigJobSeeker}/>
+
+           </div>
+
+           <div className="div-personal-form-data">
 
            </div>
 
--- src/pages/FormData/Form/Form.jsx
+++ src/pages/FormData/Form/Form.jsx
@@ -1,16 +1,24 @@
 import './Form.css'
 import formData from './JsonDataForm/formProfileJobseeker.json'
 import Input from "./Input/Input.jsx";
+import Button from "../../globalcomponents/Buttons/Button.jsx";
+import React from "react";
 
 
-function Form() {
+// eslint-disable-next-line react/prop-types
+function Form({formConfig}) {
+
+    const mainTitle = formConfig[0].maintitel;
+    const inputFields = formConfig.slice(1);
 
 
     return (
 
         <form className="global-form-style">
 
-            {formData.map((formData, index) => (
+            <h3 className='h-maintitel'>{mainTitle}</h3>
+
+            {inputFields.map((formData, index) => (
 
                 <Input
                 key={index}
@@ -20,9 +28,11 @@ function Form() {
                 placeholder={formData.placeholder}
                 />
                 ))}
-
+            <Button text="verzenden"></Button>
 
      </form>
+
+
 
     )
 }
